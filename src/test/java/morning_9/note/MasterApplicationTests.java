32c65e594293b7713842dd35c04fe38910328981
weniseb <sebastian.weninger@gmail.com>
2020-03-18 09:28:03 +0100

    init

---
+++ src/test/java/morning_9/note/MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package morning_9.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
